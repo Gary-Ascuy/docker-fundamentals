d4901c19be4919a33306cd1753255769fe01073b
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-11-19 18:26:27 -0400

    Adding random chart for soul

--- astral/client/templates/soul/soul_status.js
+++ astral/client/templates/soul/soul_status.js
@@ -2,9 +2,66 @@
 const leftPad = (number, n) => `00000${number}`.substr(-n);
 
 Template.soulStatus.onRendered(function() {
+
+
+
   setInterval(() => {
     Session.set('something', new Date());
+    chart = new Chartist.Line('#soulchart', {
+      labels: ['Jan', 'Feb', 'Mar'],
+      series: [
+        [5, 3, 10, Math.random() * 10],
+        [5, 3, 3, Math.random() * 10],
+        [5, 3, 1, Math.random() * 10]
+      ]
+    }, {
+      showArea: true,
+      axisY: {
+        onlyInteger: true
+      }
+    });
+
+  //   const ctx = document.getElementById('soulchart').getContext("2d");
+  //   const myChart = new Chart(ctx, {
+  //     type: 'bar',
+  //     data: {
+  //         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  //         datasets: [{
+  //             label: '# of Votes',
+  //             data: [12, 19, 3, 5, 2, 3],
+  //             backgroundColor: [
+  //                 'rgba(255, 99, 132, 0.2)',
+  //                 'rgba(54, 162, 235, 0.2)',
+  //                 'rgba(255, 206, 86, 0.2)',
+  //                 'rgba(75, 192, 192, 0.2)',
+  //                 'rgba(153, 102, 255, 0.2)',
+  //                 'rgba(255, 159, 64, 0.2)'
+  //             ],
+  //             borderColor: [
+  //                 'rgba(255,99,132,1)',
+  //                 'rgba(54, 162, 235, 1)',
+  //                 'rgba(255, 206, 86, 1)',
+  //                 'rgba(75, 192, 192, 1)',
+  //                 'rgba(153, 102, 255, 1)',
+  //                 'rgba(255, 159, 64, 1)'
+  //             ],
+  //             borderWidth: 1
+  //         }]
+  //     },
+  //     options: {
+  //         scales: {
+  //             yAxes: [{
+  //                 ticks: {
+  //                     beginAtZero:true
+  //                 }
+  //             }]
+  //         }
+  //     }
+  // });
+
   }, 2000);
+
+
 });
 
 Template.soulStatus.helpers({
