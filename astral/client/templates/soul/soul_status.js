5f88607324c1a1756a3a1ef2d29f34be1e780cc8
Gary Ascuy <gary.ascuy@jalasoft.com>
2016-11-19 17:26:42 -0400

    Updating soul status

--- astral/client/templates/soul/soul_status.js
+++ astral/client/templates/soul/soul_status.js
@@ -1,16 +1,25 @@
 
 const leftPad = (number, n) => `00000${number}`.substr(-n);
 
+Template.soulStatus.onRendered(function() {
+  setInterval(() => {
+    Session.set('something', new Date());
+  }, 2000);
+});
+
 Template.soulStatus.helpers({
   soul() {
+    Session.get('something');
     const soulContext = Math.sin(Math.random())  * 10000;
     return leftPad(parseInt(soulContext), 5);
   },
   status() {
+    Session.get('something');
     const statusContext = Math.cos(Math.random()) * 80000;
     return leftPad(parseInt(statusContext), 5);
   },
   universe() {
+    Session.get('something');
     const universeContext = Math.tan(Math.random()) * 10000;
     return leftPad(parseInt(universeContext), 5);
   }
